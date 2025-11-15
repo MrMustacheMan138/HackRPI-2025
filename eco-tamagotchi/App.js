560bb691c504400ee84b2df35596d22692f2a1e7
jaylizhao <zhaoj17@rpi.edu>
2025-11-15 14:06:35 -0500

    Front-end testing

---
+++ eco-tamagotchi/App.js
@@ -0,0 +1,7 @@
+// App.js (temporary for testing)
+import React from 'react';
+import EcoDebugScreen from './src/screens/ecodebugscreen';
+
+export default function App() {
+  return <EcoDebugScreen />;
+}
