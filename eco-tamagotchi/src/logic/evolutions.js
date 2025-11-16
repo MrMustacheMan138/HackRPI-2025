538ed8b3d49408d10a2b1d6e5568b93963a2815b
jaylizhao <zhaoj17@rpi.edu>
2025-11-16 01:11:17 -0500

    Fixing evolution timing

--- eco-tamagotchi/src/logic/evolutions.js
+++ eco-tamagotchi/src/logic/evolutions.js
@@ -2,9 +2,9 @@
 
 export const PET_EVOLUTION_STAGES = [
   { name: 'Egg',        minLevel: 1, maxLevel: 16 },
-  { name: 'Blob',       minLevel: 16, maxLevel: 32 },
-  { name: 'Tree Guardian', minLevel: 32, maxLevel: 64 },
-  { name: 'Forest Spirit', minLevel: 64, maxLevel: 99 },
+  { name: 'Blob',       minLevel: 15, maxLevel: 32 },
+  { name: 'Tree Guardian', minLevel: 31, maxLevel: 64 },
+  { name: 'Forest Spirit', minLevel: 63, maxLevel: 99 },
 ];
 
 export function getStageForLevel(level) {
